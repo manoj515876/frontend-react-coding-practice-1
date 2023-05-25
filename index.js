5578f5e589e6f45c90e99bb702670e6d79c48243
Manoj Mantha <manojputtaluckey@gmail.com>
2023-05-25 13:35:07 +0000

    React Coding Practice 1

---
+++ index.js
@@ -0,0 +1,17 @@
+const element = (
+  <div className="container">
+    <h1 className="heading">Super Over League</h1>
+    <div className="teams-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        className="image-logo"
+      />
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        className="image-logo"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
